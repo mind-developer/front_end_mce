82e3d657514e0362259f3f9de9d41f998022da25
Vinicius Leal <56416018+viniciusleal34@users.noreply.github.com>
2021-07-16 11:50:26 -0300

    fix: listagem de agendamento

--- src/views/pages/Horario/index.js
+++ src/views/pages/Horario/index.js
@@ -16,11 +16,11 @@ import pt from "date-fns/locale/pt";
 
 import { Container, Content, Time } from "./styles";
 import api from "../../../services/api";
+import addHours from "date-fns/addHours";
 
 const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 function Agenda() {
-  const hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
 
@@ -45,21 +45,20 @@ function Agenda() {
 
   useEffect(() => {
     async function loadSchedule() {
-      const response = await api.get("/schedule", {
-        params: { date: "2021-07-20" },
-      });
+      const response = await api.get("/schedule", { params: { date } });
 
       const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
       const data = range.map((hour) => {
         const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
         const compareDate = utcToZonedTime(checkDate, timezone);
+        // console.log(checkDate, compareDate);
 
         return {
           time: `${hour}:00h`,
           past: isBefore(compareDate, new Date()),
-          appointment: response.data.find((a) =>
-            isEqual(parseISO(a.date), compareDate)
+          appointment: response.data.find(
+            (a) => a.date.split("T")[1]?.split(":")[0] == hour
           ),
         };
       });
@@ -90,22 +89,22 @@ function Agenda() {
         </button>
       </header>
       <ul>
-        {schedule?.map((time, index) =>
-          time?.appointment?.user?.name ? (
+        {schedule?.map((time, index) => {
+          return time.appointment ? (
             <Content>
-              <div>{hours[index]}h</div>
+              <div>{range[index]}h</div>
               <Time>
-                <strong>Adam Sander</strong>
-                <span>(11) 99789-1431</span>
+                <strong>{time.appointment?.user?.name}</strong>
+                {/* <span>(11) 99789-1431</span> */}
               </Time>
             </Content>
           ) : (
             <Content>
-              <div>{hours[index]}h</div>
+              <div>{range[index]}h</div>
               <span></span>
             </Content>
-          )
-        )}
+          );
+        })}
       </ul>
     </Container>
   );
